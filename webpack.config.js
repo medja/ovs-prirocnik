db3b704edfcf4a200c8779c6056f21c04bb7fc5e
Metod Medja <metod@medja.net>
2016-04-03 20:53:48 +0000

    fix(webpack): switch to realtive urls

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.join(__dirname, 'build'),
-        publicPath: '/build/',
+        publicPath: 'build/',
         filename: 'bundle.js'
     },
     module: {
