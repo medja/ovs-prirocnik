ab1ea37cb37815744a692a44a11664ec2f51c71f
Metod Medja <metod@medja.net>
2016-04-07 08:48:16 +0000

    feat(content): add distribution and density function chapters

--- src/chapters/04-porazdelitve-verjetnosti/01-funkcija-gostote/index.js
+++ src/chapters/04-porazdelitve-verjetnosti/01-funkcija-gostote/index.js
@@ -18,7 +18,21 @@ function Chapter() {
     return (
         <div>
             <p>
-                ...
+                Funkcija gostote nam omogoča računanje verjetnosti slučajev
+                spremenljivke, <strong>če poznamo njeno porazdelitev</strong>.
+                Njeno vrednost lahko razberemo tudi s pomočjo verjetnostne
+                tabele, saj je njena vrednost za neki <Equation math="x" />{' '}
+                natanko vrednost v tabeli pod tem <Equation math="x" />-om,
+                torej <Equation math="p_x" />.
+            </p>
+            
+            <p>
+                Ta funkcija nam zato pride prav, ko ne poznamo vnaprej
+                verjetnosti izidov oziroma kadar potrebujemo enačbo za izračun
+                le teh. Ker ta funkcija vrača relativno gostoto oziroma
+                verjetnost nekega izida pa seveda more biti omejena. Ker nič ne
+                more biti več kot <Equation math="100\%" /> mora biti vsota vseh
+                vrednosti te funkcije vedno enaka <Equation math="1" />.
             </p>
         </div>
     );
--- src/chapters/04-porazdelitve-verjetnosti/02-porazdelitvena-funkcija/index.js
+++ src/chapters/04-porazdelitve-verjetnosti/02-porazdelitvena-funkcija/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import { createChapter } from 'components/chapter';
 import Equation from 'components/equation';
+import Formula from 'components/formula';
+import { Continuous, Discrete } from 'components/plot';
 
 const title = 'Porazdelitvena funkcija';
 
@@ -18,7 +20,55 @@ function Chapter() {
     return (
         <div>
             <p>
-                ...
+                Porazdelitvena funkcija je tesno povezana s funkcijo gostote. Če
+                smo pri funkciji gostote računali verjetnost nekega izida{' '}
+                <Equation math="x" />, zdaj računamo vsoto vseh izidov manjših
+                ali enakih <Equation math="x" />. Kot pri funkciji gostote nam
+                za računanje s to funkcijo pri končnem številu izidov ni
+                potrebno uporabiti nobene posebne formule. Samo sešteti moramo
+                vse verjetnosti, ki ustrezajo pogoju, torej{' '}
+                <Equation math="P(X \le x)" />. V nasprotnem primeru pa stvari
+                postanejo nekoliko bolj zapletene.
+            </p>
+            
+            <Formula
+                name="Porazdelitvena funkcija (diskretna)"
+                math="F(x) = \sum_{i=1}^x p(i)"
+            />
+            
+            <p style={{ textAlign: 'center' }}>
+                <Discrete
+                    fill="#bbdefb" style={{ maxWidth: '500px' }}
+                    range={[1, 5]} selected={[1, 3]}
+                    func="binomial(4, x) * (1 / 2)^4"
+                />
+            </p>
+            
+            <p>
+                Pri diskretnih porazdelitvah funkcija predstavlja vsoto vseh
+                verjetnosti do <Equation math="c" />. Ker je navadno prvi možni
+                izid <Equation math="1" />, je to tudi spodnja meja zgornje
+                vsote. Ta del vsote je po potrebi treba spremeniti, da ustreza
+                sami porazdelitvi.
+            </p>
+            
+            <Formula
+                name="Porazdelitvena funkcija (zvezna)"
+                math="F(x) = \int_{-\infty}^x p(i)"
+            />
+            
+            <p style={{ textAlign: 'center' }}>
+                <Continuous
+                    fill="#bbdefb" style={{ maxWidth: '500px' }}
+                    range={[-1, 3]} selected={[-2, 1]}
+                    func="exp(- x^2 / 2) / sqrt(2 * pi)"
+                />
+            </p>
+            
+            <p>
+                Pri zveznih porazdelitvah funkcija predstavlja površino, ki si
+                jo je mogoče predstavljati kot vsoto verjetnosti na neskončno
+                majhnih intervalih.
             </p>
         </div>
     );
