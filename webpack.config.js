f8abdf772f83522ca18c1801e1cb658093b0f214
Metod Medja <metod@medja.net>
2016-04-09 21:53:59 +0000

    fix(webpack): fix environment variable definition

--- webpack.config.js
+++ webpack.config.js
@@ -58,9 +58,7 @@ module.exports = {
     },
     plugins: [
         new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: process.env.NODE_ENV
-            }
+            'process.env.NODE_ENV': '"process.env.NODE_ENV"'
         })
     ]
 };
